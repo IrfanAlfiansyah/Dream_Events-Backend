da41c0b9f5caa32afc693a470cfa1a7030e9bfe8
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-09-15 10:48:13 +0700

    add middleware

--- src/routes/wishlist.js
+++ src/routes/wishlist.js
@@ -3,10 +3,27 @@ const express = require("express");
 const Router = express.Router();
 
 const wishlistController = require("../controllers/wishlist");
+const authMiddleware = require("../middleware/auth");
 
-Router.get("/", wishlistController.getAllWishlist);
-Router.get("/:wishlistId", wishlistController.getWishlistById);
-Router.post("/", wishlistController.createWishlist);
-Router.delete("/:wishlistId", wishlistController.deleteWishlist);
+Router.get(
+  "/",
+  authMiddleware.authentication,
+  wishlistController.getAllWishlist
+);
+Router.get(
+  "/:wishlistId",
+  authMiddleware.authentication,
+  wishlistController.getWishlistById
+);
+Router.post(
+  "/",
+  authMiddleware.authentication,
+  wishlistController.createWishlist
+);
+Router.delete(
+  "/:wishlistId",
+  authMiddleware.authentication,
+  wishlistController.deleteWishlist
+);
 
 module.exports = Router;
