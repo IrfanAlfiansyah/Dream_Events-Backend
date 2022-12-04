e3423f174732a5d552ef012603f223ca42aa4818
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-12-04 15:29:08 +0700

    update

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -25,45 +25,4 @@ Router.patch(
   bookingController.updateBookingStatus
 );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-app.post("/payment", (req, res) => {
-  const dataUser = {
-    fullName: "test doang",
-  };
-
-  const dataTransaction = {
-    amount: "Rp 5000",
-  };
-
-  const condition = "'stock-GOOG' in topics || 'industry-tech' in topics";
-
-  const message = {
-    notification: {
-      title: `payment success, kamu udah ngirim ke ${dataUser.fullName}`,
-      body: `kamu mengirim sebesar${dataTransaction.amount}.`,
-    },
-    condition,
-  };
-
-  var admin = require("firebase-admin");
-
-  var serviceAccount = require("./resources/dream-events-365800-firebase-adminsdk-qssv8-9c172e029d.json");
-
-  // Send a message to devices subscribed to the combination of topics
-  // specified by the provided condition.
-  getMessaging()
-    .send(message)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log("Successfully sent message:", response);
-    })
-    .catch((error) => {
-      console.log("Error sending message:", error);
-    });
-}); 
-
-
 module.exports = Router;
